1d8af87860f9a65d6fd0b447497d372ac340e9d7
jharnam <jgulrajani@babbel.com>
2020-09-16 22:07:47 +0200

    Client->GCE->MyJavaLibrary done

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -10,14 +12,26 @@ import android.widget.Toast;
 
 import com.example.android.jandroidlibrary.JAndroidJokeDisplayActivity;
 import com.example.jjokegeneratorlib.JJokeGenerator;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+import com.udacity.gradle.builditbigger.backend.myApi.model.MyBean;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+    String joke;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //new JokesEndpointAsyncTask().execute(new Pair<Context, String>(this, "Jharna"));
+        new JokesEndpointAsyncTask().execute();
     }
 
 
@@ -54,6 +68,84 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, JAndroidJokeDisplayActivity.class);
         startActivity(intent);
     }
+
+    public void displayJokeViaGCEAndJavaLib(View view) {
+        new JokesEndpointAsyncTask().execute();
+        //new JokesEndpointAsyncTask().execute(new Pair<Context, String>(this, "Jharna"));
+    }
+
+    private void updateUI(String result) {
+        joke = result;
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public class JokesEndpointAsyncTask  extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+        /*
+
+        if (params.length < 1 || params[0] == null) {
+            Log.i("Jharna", "here");
+            return null;
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+         */
+
+            try {
+                //return myApiService.sayHi(name).execute().getData();
+                return myApiService.getJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        /**
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
+         *
+         * It IS okay to modify the UI within this method. We take the {@link MyBean} object
+         * (which was returned from the doInBackground() method) and update the views on the screen.
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            if (result == null) {
+                //We cannot do any updating, as nothing was returned.
+                return ;
+            }
+            //Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+            //TODO update the UI
+            updateUI(result);
+
+        }
+    }
+
+
 
 
 }
--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.jjokegeneratorlib.JJokeGenerator;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -19,12 +20,23 @@ import javax.inject.Named;
 public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
+
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
         return response;
+    }
+
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+        JJokeGenerator jokeGenerator = new JJokeGenerator();
+        response.setData(jokeGenerator.getJoke());
+
+        return response;
+
     }
 
 }
