17d0a827500fb52639a181049992302e4869a12d
jharnam <jgulrajani@babbel.com>
2020-09-21 22:10:19 +0200

    google ad dependency only for free version

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -11,7 +11,6 @@ import com.udacity.gradle.builditbigger.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MainActivityFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class MainActivityFragment extends Fragment {
@@ -28,41 +27,6 @@ public class MainActivityFragment extends Fragment {
 
             return root;
         }
-
-    /*
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-     */
-
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainActivityFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    /*
-    public static MainActivityFragment newInstance(String param1, String param2) {
-        MainActivityFragment fragment = new MainActivityFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-     */
 
 
 }
